e632a2cc7d04c0ea87cecbc0b8fff3ec333108ca
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-04-08 11:52:40 +0330

    add file

---
+++ question.js
@@ -0,0 +1,173 @@
+/*
+1- What is Node.js?
+
+A. A front-end framework
+B. A back-end framework
+C. A JavaScript runtime built on Chrome's V8 JavaScript engine 
+D. A type of database
+
+===========================================================
+2- Which package manager is bundled with Node.js by default?
+
+A. npm 
+B. bower
+C. yarn
+D. grunt
+
+===========================================================
+3- Which of the below options is correct about package.json in the node.js application?
+
+A. It is present in the root directory of any Node application.
+B. It is used to define the properties of a package.
+C. It is used to update the dependencies of an application.
+D. All of the above 
+
+===========================================================
+4- Which of the following command will show all the modules installed globally?
+
+A. $ npm ls -g  
+B. $ npm ls
+C. $ node ls -g
+D. $ node ls
+
+===========================================================
+5- How can you initiate a project using npm?
+
+A. npm start
+B. npm init 
+C. npm run
+D. npm create
+
+===========================================================
+6- Which of the below is the correct syntax for installing the Nodemon in the Node.js application ?
+
+A. npm install nodemon 
+B. node install nodemon
+C. npm install node
+D. node install -l nodemon
+
+===========================================================
+7- Which of the following is a core module for handling paths?
+
+A. url
+B. dir
+C. path 
+D. location
+
+===========================================================
+8- Which global object provides functionality to control the Node.js runtime process?
+
+A. Global
+B. Runtime
+C. Process 
+D. NodeControl
+
+===========================================================
+9- How do you install a package locally using npm in a Node.js application?
+
+A. npm install -g <package-name>
+B. npm local install <package-name>
+C. npm run install <package-name>
+D. npm install <package-name> 
+
+===========================================================
+10-  What is the purpose of the --save flag in the npm install command?
+
+A. To globally install a package
+B. To save a backup of the current project
+C. To save the package version in the package-lock.json
+D. To save the package in the package.json dependencies 
+
+===========================================================
+11- How can you read command-line arguments in a Node.js application?
+
+A. argv
+B. process.args
+C. arguments
+D. process.argv 
+
+===========================================================
+12- Which core module in Node.js provides asynchronous file operations?
+
+A. http
+B. url
+C. fs 
+D. path
+
+
+===========================================================
+13- Which npm command is used to install all dependencies listed in the package.json file?
+
+A. npm init
+B. npm run
+C. npm start
+D. npm install 
+
+===========================================================
+14- What is the main difference between exports and module.exports in Node.js?
+
+A. They are the same.
+B. exports is for functions, while module.exports is for objects.
+C. exports is a reference to module.exports. 
+D. module.exports is the legacy way to export modules.
+
+===========================================================
+15- Which method is used in Node.js to import modules?
+
+A. require() 
+B. include()
+C. import
+D. fetch()
+
+===========================================================
+16- Which of the following is used to import modules in Node.js?
+
+A. import { module } from 'module-name'
+B. #include 'module-name'
+C. require('module-name') 
+D. using module-name
+
+===========================================================
+17- What is the default scope in the Node.js application ?
+
+A. global 
+B. local 
+C. global Function
+D. local to object
+
+===========================================================
+18- Which Node.js command is used to execute a JavaScript file, say "app.js"?
+
+A. node install app.js
+B. node run app.js
+C. node app.js 
+D. npm app.js
+
+
+
+===========================================================
+19- Which Node.js method is used to delay the execution of a function?
+
+A. process.delay()
+B. setTimeout() 
+C. wait()
+D. sleep()
+
+===========================================================
+20- What does the npm init command do?
+
+A. It installs all the dependencies listed in package.json.
+B. It starts the Node.js application.
+C. It creates a new Node.js application.
+D. It initializes a new package.json file. 
+
+===========================================================
+21- Which method in the fs module is used to read a file asynchronously?
+
+A. fs.readFile() 
+B. fs.readSync()
+C. fs.openFile()
+D. fs.read()
+
+===========================================================
+*/
